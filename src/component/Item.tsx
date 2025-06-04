50e6d5a3683e31653e085626614c04fdd243fafd
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-06-05 01:16:48 +0530

    Get loaded all reports for update the state

--- src/component/Item.tsx
+++ src/component/Item.tsx
@@ -1,49 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
-import { GetAllReports} from "../service/Item/Item";
+import { GetAllReports } from "../service/Item/Item";
 import Dropdown from 'react-bootstrap/Dropdown';
 
-export const Item=()=>{
-    const tHeadings:String[]=[
-        "ReportID","UserID","Item Name","Category","Description","Brand","Colour","Found Date","Last Seen Date","Found Location","Last Seen Location","Private Details","Status"
+export const Item = () => {
+    const tHeadings: String[] = [
+        "ReportID", "UserID", "Item Name", "Category", "Description", "Brand", "Colour", "Found Date", "Last Seen Date", "Found Location", "Last Seen Location", "Private Details", "Status"
     ]
 
-    useEffect(()=>{
-        const loadData=async()=>{
-            const getAllReports=await GetAllReports()
-            console.log("Get All Reports",getAllReports)
+    enum ItemCategory {
+        STATIONERY, KEYS, ELECTRONICS, ID_CARDS
+    }
+    enum ItemStatus {
+        LOST, FOUND, CLAIMED
+    }
+
+    interface Report {
+        reportId:String;
+        userId: String;
+        itemName: String;
+        category: ItemCategory;
+        description: String;
+        brand: String;
+        colour: String;
+        foundDate: String;
+        lastSeenDate: String;
+        foundLocation: String;
+        lastSeenLocation: String;
+        privateDetails: String;
+        status:ItemStatus;
+    }
+
+    const[reports,setReports]=useState<Report[]>([])
+
+    useEffect(() => {
+        const loadData = async () => {
+            const getAllReports = await GetAllReports()
+            setReports(getAllReports)
+            console.log("Get All Reports", getAllReports)
         };
         loadData();
-    },[])
+    }, [])
 
-     return (
-    <Table striped bordered hover variant="dark" style={{position:"absolute",top:"130px"}}>
-      <thead>
-        <tr>
-          {tHeadings.map((headings)=>(
-            <th>{headings}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {/* <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr> */}
-      </tbody>
-    </Table>
-  );
+    return (
+        <Table striped bordered hover variant="dark" style={{ position: "absolute", top: "130px" }}>
+            <thead>
+                <tr>
+                    {tHeadings.map((headings) => (
+                        <th>{headings}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                   
+            </tbody>
+        </Table>
+    );
 }
