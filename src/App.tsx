295ab9e11603e733f8642ae2340f881bdbdfe857
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-06-04 19:40:32 +0530

    Add table for Item and mapping <th> s

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/NavBar';
+import { Item } from './component/Item';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Item/>
     </div>
   );
 }
--- src/component/Item.tsx
+++ src/component/Item.tsx
@@ -1,3 +1,37 @@
+import { Table } from "react-bootstrap";
+
 export const Item=()=>{
-    
+    const tHeadings:String[]=[
+        "ReportID","UserID","Item Name","Category","Description","Brand","Colour","Found Date","Last Seen Date","Found Location","Last Seen Location","Private Details","Status"
+    ]
+     return (
+    <Table striped bordered hover variant="dark">
+      <thead>
+        <tr>
+          {tHeadings.map((headings)=>(
+            <th>{headings}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {/* <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr> */}
+      </tbody>
+    </Table>
+  );
 }
