afe2aaca0fcd1981a0824d948635bca1c46695c2
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-06-08 10:34:30 +0530

    Handle  a dropdown for show all items of a selected group

--- src/component/Item.tsx
+++ src/component/Item.tsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
 import { Table } from "react-bootstrap";
-import { GetAllReports } from "../service/Item/Item";
+import { GetAllItemsOfSelectedGroup, GetAllReports } from "../service/Item/Item";
 import Dropdown from 'react-bootstrap/Dropdown';
 
 export const Item = () => {
     const tHeadings: String[] = [
-        "ReportID","UserID", "Item Name", "Category", "Description", "Brand", "Colour", "Found Date", "Last Seen Date", "Found Location", "Last Seen Location", "Private Details", "Status"
+        "ReportID", "UserID", "Item Name", "Category", "Description", "Brand", "Colour", "Found Date", "Last Seen Date", "Found Location", "Last Seen Location", "Private Details", "Status"
     ]
 
     enum ItemCategory {
         STATIONERY, KEYS, ELECTRONICS, ID_CARDS
     }
     enum ItemStatus {
-        LOST, FOUND, CLAIMED
+        LOST = "LOST",
+        FOUND = "FOUND",
+        CLAIMED = "CLAIMED"
     }
 
     interface Report {
-        reportId:string;
+        reportId: string;
         userId: string;
         itemName: string;
         category: ItemCategory;
@@ -28,37 +30,60 @@ export const Item = () => {
         foundLocation: string;
         lastSeenLocation: string;
         privateDetails: string;
-        status:ItemStatus;
+        status: ItemStatus;
     }
 
-    const[reports,setReports]=useState<Report[]>([])
+    const [reports, setReports] = useState<Report[]>([])
+
+    const loadData = async () => {
+        const getAllReports = await GetAllReports()
+        setReports(getAllReports)
+        console.log("Get All Reports", getAllReports)
+    }
 
     useEffect(() => {
-        const loadData = async () => {
-            const getAllReports = await GetAllReports()
-            setReports(getAllReports)
-            console.log("Get All Reports", getAllReports)
-        };
         loadData();
     }, [])
 
+    const handleDropdown = async (itemStatus: ItemStatus) => {
+        const getAllItemsOfSelectedGroup = await GetAllItemsOfSelectedGroup(itemStatus)
+        setReports(getAllItemsOfSelectedGroup)
+        console.log(getAllItemsOfSelectedGroup)
+    }
+
+
+
     return (
-        <Table striped bordered hover variant="dark" style={{ position: "absolute", top: "130px" }}>
-            <thead>
-                <tr>
-                    {tHeadings.map((headings) => (
-                        <th>{headings}</th>
-                    ))}
-                </tr>
-            </thead>
-            <tbody>
-                {reports.map((row)=>(
-                <tr key={row.reportId}>
-                    {Object.values(row).map((cell,index)=>(
-                        <td key={index}>{cell}</td>
-                    ))}
-                </tr>))}  
-            </tbody>
-        </Table>
+        <>
+            <Dropdown style={{ position: "absolute", top: "75px", right: "0px" }}>
+                <Dropdown.Toggle variant="info" id="dropdown-basic">
+                    Item Status
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-3" onClick={() => loadData()}>All</Dropdown.Item>
+                    <Dropdown.Item href="#/action-1" onClick={() => handleDropdown(ItemStatus.FOUND)}>Found</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2" onClick={() => handleDropdown(ItemStatus.LOST)}>Lost</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3" onClick={() => handleDropdown(ItemStatus.CLAIMED)}>Claimed</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+            <Table striped bordered hover variant="dark" style={{ position: "absolute", top: "130px" }}>
+                <thead>
+                    <tr>
+                        {tHeadings.map((headings) => (
+                            <th>{headings}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {reports.map((row) => (
+                        <tr key={row.reportId}>
+                            {Object.values(row).map((cell, index) => (
+                                <td key={index}>{cell}</td>
+                            ))}
+                        </tr>))}
+                </tbody>
+            </Table>
+        </>
     );
 }
--- src/service/Item/Item.ts
+++ src/service/Item/Item.ts
@@ -1,12 +1,25 @@
 import axios from "axios"
+const baseUrl="http://localhost:8050/LostAndFound/api/v1/item"
 
 enum ItemStatus {
-    LOST, FOUND, CLAIMED
-}
+        LOST="LOST",
+        FOUND="FOUND",
+        CLAIMED="CLAIMED"
+    }
 
 export const GetAllReports = async () => {
     try {
-        const response = await axios.get('http://localhost:8050/LostAndFound/api/v1/item/getAllReports');
+        const response = await axios.get(`${baseUrl}/getAllReports`);
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const GetAllItemsOfSelectedGroup=async(itemStatus:ItemStatus)=>{
+    try {
+        const response=await axios.get(`${baseUrl}/getAll?itemStatus=${itemStatus}`);
+        console.log(response.data)
         return response.data
     } catch (error) {
         console.error(error)
