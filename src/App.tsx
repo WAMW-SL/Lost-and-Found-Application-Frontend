9dc4b330e027beeefb6698bb7df6c5e6c5d6e12e
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-06-21 01:04:37 +0530

    Add table for Request and mapping <th> s

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/NavBar';
 import { Item } from './component/item/Item';
+import { Request } from './component/request/Request';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <Item/>
+      {/* <Item/> */}
+      <Request/>
     </div>
   );
 }
