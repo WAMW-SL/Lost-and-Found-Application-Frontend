dc46c4e81f1b8e7bad3200b798738ca931528c81
WAMAW-SL <amanthamihiran2003@gmail.com>
2025-06-21 03:05:14 +0530

    Handle  a dropdown for show all requests of a selected group

--- src/component/request/Request.tsx
+++ src/component/request/Request.tsx
@@ -1,53 +1,74 @@
 import { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
-import { GetAllRequests } from "../../service/Request/Request";
+import { Dropdown, Table } from "react-bootstrap";
+import { GetAllRequests, GetAllRequestsOfSelectedGroup } from "../../service/Request/Request";
 
-enum RequestStatus{
-  PENDING="PENDING",
-  APPROVED="APPROVED",
-  REJECTED="REJECTED"
+enum RequestStatus {
+  PENDING = "PENDING",
+  APPROVED = "APPROVED",
+  REJECTED = "REJECTED"
 }
 
-interface Request{
-  requestId:string
-  fullDescription:string
-  requestStatus:RequestStatus
-  userId:string
+interface Request {
+  requestId: string
+  fullDescription: string
+  requestStatus: RequestStatus
+  userId: string
 }
 
-export const Request=()=>{
-    const tHeadings:String[]=[
-        "Request ID","Full Description","Status","User ID"
-    ]
+export const Request = () => {
+  const tHeadings: String[] = [
+    "Request ID", "Full Description", "Status", "User ID"
+  ]
 
-    const[requests,setRequests]=useState<Request[]>([])
+  const [requests, setRequests] = useState<Request[]>([])
 
-    useEffect(()=>{
-        const loadData=async()=>{
-            const getAllRequests=await GetAllRequests()
-            setRequests(getAllRequests)
-            console.log("Get All Requests",getAllRequests)
-        };
-        loadData();
-    },[])
+  const loadData = async () => {
+    const getAllRequests = await GetAllRequests()
+    setRequests(getAllRequests)
+    console.log("Get All Requests", getAllRequests)
+  }
 
-     return (
-    <Table striped bordered hover variant="dark" style={{position:"absolute",top:"130px"}}>
-      <thead>
-        <tr>
-          {tHeadings.map((headings)=>(
-            <th>{headings}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {requests.map((row)=>(
-                <tr key={row.requestId}>
-                    {Object.values(row).map((cell,index)=>(
-                        <td key={index}>{cell}</td>
-                    ))}
-                </tr>))}  
-      </tbody>
-    </Table>
+  useEffect(() => {
+    loadData();
+  }, [])
+
+  const handleDropdown = async (requestStatus: RequestStatus) => {
+    const getAllRequestsOfSelectedGroup = await GetAllRequestsOfSelectedGroup(requestStatus)
+    setRequests(getAllRequestsOfSelectedGroup)
+    console.log(getAllRequestsOfSelectedGroup)
+  }
+
+  return (
+    <>
+      <Dropdown style={{ position: "absolute", top: "75px", right: "0px" }}>
+        <Dropdown.Toggle variant="info" id="dropdown-basic">
+          Request Status
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-3" onClick={() => loadData()}>All</Dropdown.Item>
+          <Dropdown.Item href="#/action-1" onClick={() => handleDropdown(RequestStatus.PENDING)}>PENDING</Dropdown.Item>
+          <Dropdown.Item href="#/action-2" onClick={() => handleDropdown(RequestStatus.APPROVED)}>APPROVED</Dropdown.Item>
+          <Dropdown.Item href="#/action-3" onClick={() => handleDropdown(RequestStatus.REJECTED)}>REJECTED</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+      <Table striped bordered hover variant="dark" style={{ position: "absolute", top: "130px" }}>
+        <thead>
+          <tr>
+            {tHeadings.map((headings) => (
+              <th>{headings}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {requests.map((row) => (
+            <tr key={row.requestId}>
+              {Object.values(row).map((cell, index) => (
+                <td key={index}>{cell}</td>
+              ))}
+            </tr>))}
+        </tbody>
+      </Table>
+    </>
   );
 }
--- src/service/Request/Request.ts
+++ src/service/Request/Request.ts
@@ -2,9 +2,25 @@ import axios from "axios";
 
 const baseUrl="http://localhost:8050/LostAndFound/api/v1/request"
 
+enum RequestStatus{
+  PENDING="PENDING",
+  APPROVED="APPROVED",
+  REJECTED="REJECTED"
+}
+
 export const GetAllRequests=async()=>{
     try {
         const response=await axios.get(`${baseUrl}/getAll`);
+        console.log(response.data)
+        return response.data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const GetAllRequestsOfSelectedGroup=async(requestStatus:RequestStatus)=>{
+    try {
+        const response=await axios.get(`${baseUrl}/getAll/${requestStatus}`);
         console.log(response.data)
         return response.data
     } catch (error) {
